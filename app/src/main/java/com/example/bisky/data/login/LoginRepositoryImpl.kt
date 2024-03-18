37e03a9511f4c5c23214f8aabdf34e3d17977549
Daniil <danil-gilin@mail.ru>
2024-03-18 14:12:24 +0300

    fix: interface LoginRepository

--- app/src/main/java/com/example/bisky/data/login/LoginRepositoryImpl.kt
+++ app/src/main/java/com/example/bisky/data/login/LoginRepositoryImpl.kt
@@ -34,5 +34,6 @@ class LoginRepositoryImpl(
                 throw e
             }
         }
+        Unit
     }
 }
--- app/src/main/java/com/example/bisky/domain/repository/login/LoginRepository.kt
+++ app/src/main/java/com/example/bisky/domain/repository/login/LoginRepository.kt
@@ -4,7 +4,7 @@ import com.example.bisky.data.network.resultwrapper.Result
 
 interface LoginRepository {
 
-    suspend fun checkSigIn(): Result<Any, Throwable>
+    suspend fun checkSigIn(): Result<Unit, Throwable>
     suspend fun sigUp(name: String, password: String, email: String): Result<Unit, Throwable>
     suspend fun sigIn(name: String, password: String): Result<Unit, Throwable>
 }
